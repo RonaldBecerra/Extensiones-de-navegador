26ce8de4a1a899e87f953be8690359aa165c5f35
Ronald <12-10706@usb.ve>
2024-03-04 17:41:51 -0400

    Intentando hacer que la cabecera de CORS se quite nuevamente

--- Skip YouTube Ads automatically/content-scripts/content.js
+++ Skip YouTube Ads automatically/content-scripts/content.js
@@ -3,26 +3,37 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	// the header "Access-Control-Allow-Origin" to any request 
 	const INVERSE_PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
-	function clickButtons(document, className){
+	function clickButtons(document, classList, functionToExecute){
 		try{
-			Array.from(document.getElementsByClassName(className)).forEach(function (button){
-				button.click();
-			});	
+			classList.forEach(function (className)
+				{
+					Array.from(document.getElementsByClassName(className)).forEach(function (button){
+						console.log("Voy a hacer click")
+						button.click();
+					});	
+				}, 
+				() => functionToExecute()
+			)
 			sendResponse({ fromcontent: "This message is from content.js" });		
 		}
 		catch(e){}
 	}
 
-	function skipAdd(className){
+	function skipAdd(classList){
 		// Try to find the button(s) in the document of the main page
-		clickButtons(document, className);
+		clickButtons(document, classList);
 
 		// Try to find the button(s) in the iframes
 		try{
 			document.querySelectorAll('iframe').forEach(frame => {
+				const frameChanged = false;
+
 				if (!frame.src.includes(INVERSE_PROXY_URL)){
+					var oldFrame = frame;
+					frameChanged = true;
+
 					// We always append the inverse proxy URL to avoid CORS errors
-					let newFrame = document.createElement("iframe");
+					var newFrame = document.createElement("iframe");
 					newFrame.setAttribute("src", INVERSE_PROXY_URL + frame.src);
 
 					// Replace the original iframe with the new one
@@ -31,11 +42,40 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 				}
 				// Try to click the buttons
 				const iframeDocument = frame.contentDocument || frame.contentWindow.document;
-				clickButtons(iframeDocument, className);
+				clickButtons(
+					iframeDocument, classList,
+					() => {
+						// We don't want to keep the changed frame
+						if frameChanged{
+							console.log("Voy a reemplazar")
+							frame.parentNode.replaceChild(oldFrame, frame);
+						}
+					}
+				);
 			});
 		}
 		catch(e){}
 	}
-	setInterval(() => skipAdd("ytp-ad-skip-button-modern"), 20);
-	setInterval(() => skipAdd("videoAdUiSkipButton"), 20);
+	setInterval(() => 
+		skipAdd([
+			"ytp-ad-skip-button-modern", // YouTube
+			"videoAdUiSkipButton", // TokyVideo
+		]), 20
+	);
 });
+
+/*
+// ESTO SIRVE PARA SIMULAR QUE SE HACE CLICK CON EL MOUSE SOBRE EL BOTÓN
+
+var rect = boton.getBoundingClientRect (); // obtenemos el rectángulo del botón
+var x = rect.left + rect.width / 2; // calculamos la coordenada X del centro del botón
+var y = rect.top + rect.height / 2; // calculamos la coordenada Y del centro del botón
+var evento = new MouseEvent ('click', { // creamos el evento
+  view: window, // la ventana es la actual
+  bubbles: true, // el evento se propaga
+  cancelable: true, // el evento se puede cancelar
+  clientX: x, // la coordenada X es la calculada
+  clientY: y // la coordenada Y es la calculada
+});
+boton.dispatchEvent (evento); // lanzamos el evento
+*/
